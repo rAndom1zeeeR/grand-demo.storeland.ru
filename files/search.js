5b569d301d8dde413305c66444c91b3f244222cf
Jora <tjorav@gmail.com>
2022-10-08 16:17:46 +0300

    [fix] 1.11

--- files/search.js
+++ files/search.js
@@ -130,7 +130,7 @@ function SearchFieldInit(obj) {
 	obj.s_reset.click(function(){
 		obj.s_search.val('').focus();
 		$('#search__result').hide();
-		$('#search__result .result__inner .result__item').remove();
+		$('#search__result .result__item').remove();
 	});
 	// Проверка данных в форме после инициализации функционала. Возможно браузер вставил туда какой-либо текст, нужно обработать и такой вариант
 	obj.__SearchFieldCheck();
@@ -279,7 +279,7 @@ $(function() {
 					}
 					// Отображаем результат поиска
 					if(с <= 3){
-						$("#search__result .result__category").append('<a class="result__item flex" href="'+ data.category[с].url +'" data-id="'+ data.category[с].goods_cat_id +'"><div class="result__image flex-center"><img src="'+ data.category[с].image_icon +'" /></div><div class="result__name"><span>'+ data.category[с].goods_cat_name +'</span></div></a>');
+						$(".result__category .result__items").append('<a class="result__item flex" href="'+ data.category[с].url +'" data-id="'+ data.category[с].goods_cat_id +'"><div class="result__image flex-center"><img src="'+ data.category[с].image_icon +'" /></div><div class="result__name"><span>'+ data.category[с].goods_cat_name +'</span></div></a>');
 					}
 				}
 			}else{
@@ -299,7 +299,7 @@ $(function() {
 					}
 					// Отображаем результат поиска
 					if(i <= 3){
-						$("#search__result .result__goods").append('<a class="result__item flex" href="'+ data.goods[i].url +'" data-id="'+ data.goods[i].goods_id +'"><div class="result__image flex-center"><img src="'+ data.goods[i].image_icon +'" /></div><div class="result__name"><span>'+ data.goods[i].goods_name +'</span></div></a>');
+						$(".result__goods .result__items").append('<a class="result__item flex" href="'+ data.goods[i].url +'" data-id="'+ data.goods[i].goods_id +'"><div class="result__image flex-center"><img src="'+ data.goods[i].image_icon +'" /></div><div class="result__name"><span>'+ data.goods[i].goods_name +'</span></div></a>');
 					}
 					// Если последняя итерация цикла вставим кнопку "показать все"
 					if(i > 3){
